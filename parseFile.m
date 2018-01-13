f77fb6c2a0a197c878de4a82a32e3253c65dd637
Shay Avital (shayavit) <shay.avital@intel.com>
2018-01-13 22:31:09 +0200

    refactor parseFile.m. usage of testID.testName instead of hard coded numbers

--- parseFile.m
+++ parseFile.m
@@ -2,29 +2,21 @@ function m = parseFile(filePath,testid, numOfLines)
 % m = PARSEFILE(filePath, testid, numOfLines) parsing the file and fetch the data according to the test we check
 %testID = getTestID(filePath);
 switch testid
-    case 0
+    case testID.standardTest
         m = dlmread(filePath,'\t',2,1);
-    case {2,3}
+    case {testID.fullLLH,testID.partialLLH1}
         m = dlmread(filePath,'\t',2,1);
         %n1 = n(:,1:metaData(3));
         %n2 = n(:,metaData(3)+1:2*metaData(3));
         %m = [n1+n2,n(:,2*metaData(3)+1:end)];
         m = m(:,2:4);
-    case 4
+    case testID.partialLLH2
         m = dlmread(filePath,'\t',2,1);
         m = m(:,3:4);
         m = [zeros(size(m,1),1),m];
-    case 5
-        %{
-        fid = fopen(filepath);
-        meta = metaData(str2int(fgets(fid)));
-        fclose(fid);
-        if meta.architecture == architecture.tlc
-            m = readErrorMapTLC(filepath,numOfLines);
-        else
-        %}
-            m = readErrorMap(filePath,numOfLines);
-        %end
+    case testID.errorMap
+        m = readErrorMap(filePath,numOfLines);
+
     otherwise
         m = zeros(1,1);
 %          err = sprintf('Error reading files.\n metaData for file:\n %s\n specifies test ID of %d which is not supported yet',...
