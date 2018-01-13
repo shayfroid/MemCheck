d65e5aef08c501ada2674cfaee1e609a58ac7bcb
Shay Avital (shayavit) <shay.avital@intel.com>
2018-01-13 19:56:48 +0200

    Implemented readErrorMApCalibrated

--- readErrorMapCalibrated.m
+++ readErrorMapCalibrated.m
@@ -20,17 +20,20 @@ wb = waitbar(0,sprintf('Reading Error Map: %d%%',0));
 m = zeros(meta.pagesPerBlock,meta.bytesPerPage*8);
 
 if (meta.architecture == architecture.mlc)
-    sums = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
-    tmp_sums = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
-elseif:
-    
+    summed_errors = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
+    %tmp_sums = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
+    pages_order = pagesOrderHynix(meta.architecture,meta.pagesPerBlock);
+    pages_coupling = reshape(pages_order,meta.pagesPerBlock/2,2);
+elseif (meta.architecture == architecture.tlc)
+    summed_errors = zeros(meta.pagesPerBlock/3,meta.bytesPerPage*8);
+    %tmp_sums = zeros(meta.pagesPerBlock/3,meta.bytesPerPage*8);
+    pages_order = [0:3:515,1:3:515,2:3:515];
+    pages_coupling = reshape(pages_order,meta.pagesPerBlock/3,3);
 end
-
-% pages order for hynix
-[left_pages,right_pages] = pagesOrderHynix(meta.architecture,meta.pagesPerBlock);
 
 iter = 0;
 while ~feof(fid)
+    m = zeros(meta.pagesPerBlock,meta.bytesPerPage*8);
     arr =  str2num(fgets(fid));
     i = 2;
     length = length(arr);
@@ -41,9 +44,42 @@ while ~feof(fid)
         i = i+arr(i)+1;
         bits = [bits,arr(i+1:i+arr(i))];
         i = i+arr(i)+1;
-        m(page+1,bits+1) = m(page+1,bits+1)+1;      
+        m(page+1,bits+1) = 1;      
     end
-            
+    
+    summed_errors_per_loop = zeros(size(summed_errors));
+    for i = 1:size(pages_coupling,1)
+       summed_errors_per_loop(i,:) = sum(m(pages_coupling(i,:)+1,:));
+    end
+    summed_errors_per_loop(summed_errors_per_loop > 1) = 1;
+    summed_errors = summed_errors + summed_errors_per_loop;
+    %{
+    if (meta.architecture == architecture.mlc)
+        for i = 1:meta.pagesPerBlock/2
+            tmp_sums(i,:) = sum(m(pages_coupling(i,:),:));
+            %tmp_sums(i,:) = m(right_pages(i)+1,:)+m(left_pages(i)+1,:);
+        end
+        sums = sums + tmp_sums;
+
+    elseif (meta.architecture == architecture.tlc)
+        triplet = 1;
+        for i = 1:3:meta.pagesPerBlock
+            tmp_sums(triplet,:) = sum(m(i:i+2,:));
+            triplet = triplet+1;
+        end
+        %{
+        for i = 1:3:meta.pagesPerBlock/3
+            temp_sums(i,:) = sum(m(i:i+2,:));
+        end
+        %}
+    else
+    % unsupporteed combination of manufacturere and architecture.
+        error_map = zeros(1,1);
+        return;        
+    end
+    %}
+    
+    
     
     iter = iter + 1;
     waitbar(double(iter/numOfLines),wb,sprintf('Reading Error Map: P/E Cycle %d/%d',iter,numOfLines));
@@ -53,69 +89,26 @@ delete(wb);
 
 fclose(fid);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% calculate pages order
-if (meta.manufacturer == manufecturers.hynix && meta.architecture == architecture.mlc)
-    % Heynix manufacturer
-    [left_pages,right_pages] = pagesOrderHynix(meta.architecture,meta.pagesPerBlock);
-elseif (meta.manufacturer == manufecturers.toshiba && meta.architecture == architecture.tlc)
-    % for TLC not using external function to calculate pages order since
-    %                      _________
-    % its straight fwd... | 0 | 258 | etc...
-    %                     |---------|
-    %             WL 0    | 1 | 259 |
-    %                     |---------|
-    %                     | 2 | 260 |
-    %                      ---------
-    %                     left | right
-    
-    %[l_plane, m_plane, u_plane] = PagesOrderToshiba(meta.architecture, meta.pagesPerBlock);
-else
-    err = sprintf('Unsupported manufacturer or architecture for BitErrorMap: In order to rander Bit Error Map graph the pages order and the coupling must be known. At the moment the the data is known for Hynix with MLC architecture or Toshiba with TLC architecture only.');
-    msgbox(err,'Unsupported manufacturer or architecture'); 
-    return;
-end
-
-if (meta.manufacturer == manufecturers.hynix && meta.architecture == architecture.mlc)
-    sums = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
+if (meta.architecture == architecture.mlc)
+    summed_errors = zeros(meta.pagesPerBlock/2,meta.bytesPerPage*8);
     for i = 1:meta.pagesPerBlock/2
         %asuming an error in both pages is not possible. 
-        sums(i,:) = m(right_pages(i)+1,:)+m(left_pages(i)+1,:);
+        summed_errors(i,:) = m(right_pages(i)+1,:)+m(left_pages(i)+1,:);
     end
-    left = sums(1:2:end,:);
-    right = sums(2:2:end,:);
+    left = summed_errors(1:2:end,:);
+    right = summed_errors(2:2:end,:);
 
-elseif (meta.manufacturer == manufecturers.toshiba && meta.architecture == architecture.tlc)
-    sums = zeros(meta.pagesPerBlock/3, meta.bytesPerPage*8);
+elseif (meta.architecture == architecture.tlc)
+    summed_errors = zeros(meta.pagesPerBlock/3, meta.bytesPerPage*8);
     for i = 1:3:meta.pagesPerBlock/3 
-        sums(i,:) = sum(m(i:i+2,:));
+        summed_errors(i,:) = sum(m(i:i+2,:));
     end
-    left = sums(1:meta.pagesPerBlock/6,:);
-    right = sums(meta.pagesPerBlock/6 + 1:end, :);
+    left = summed_errors(1:meta.pagesPerBlock/6,:);
+    right = summed_errors(meta.pagesPerBlock/6 + 1:end, :);
 else
     % unsupporteed combination of manufacturere and architecture.
     error_map = zeros(1,1);
-    return;
-           
+    return;        
 end
 error_map = [left,right];
 %disp ('writing sum');
