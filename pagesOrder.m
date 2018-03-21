11656e155e4cea9a6b2412c9d50e36f0ac1ab605
Shay Avital (shayavit) <shay.avital@intel.com>
2018-03-21 16:17:18 +0200

    modified pagesOrder to be generic and support reading for bitErrorMap

--- pagesOrder.m
+++ pagesOrder.m
@@ -7,11 +7,25 @@ md = metaData(str2num(fgets(fid)));
 numOfPages = md.pagesPerBlock;
 str = fgets(fid);
 fclose(fid);
-strarr = strsplit(str,'\t');
-strarr = strarr(2:2:(2*numOfPages));
-arr = zeros(1,size(strarr,2));
-for i = 1:size(strarr,2)
-    str = strsplit(strarr{i},'_');
-    arr(i) = str2double(str{2});
-end
 
+if (md.testID == testID.errorMap)
+    arr = str2num(str);
+   %{
+    if (md.architecture == architecture.mlc)
+        arr = reshape(arr,md.pagesPerBlock/2,2)';
+    elseif (md.architecture == architecture.tlc)
+        arr = reshape(arr,md.pagesPerBlock/3,3)';
+    else
+        err = sprintf('Unsupported aarchitecture: %d', md.architecture);
+        msgbox(err,'Unsupported architecture'); 
+    end
+   %}
+elseif(md.testID == testID.standardTest)   
+    strarr = strsplit(str,'\t');
+    strarr = strarr(2:2:(2*numOfPages));
+    arr = zeros(1,size(strarr,2));
+    for i = 1:size(strarr,2)
+        str = strsplit(strarr{i},'_');
+        arr(i) = str2double(str{2});
+    end
+end
