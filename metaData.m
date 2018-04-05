c8c906b9dac21967db97999493d4490f8621f20c
Shay Avital (shayavit) <shay.avital@intel.com>
2018-04-05 18:50:23 +0300

    fixed issue #15. (metaData assertion. now showing msg and raising exception.

--- metaData.m
+++ metaData.m
@@ -23,45 +23,32 @@ classdef metaData
                 this.numOfPECycles = -1;
 
             elseif nargin == 1
-                assert(size(varargin{1}, 2) >=7)
+                if (size(varargin{1}, 2) < 8)
+                    msgbox('Ivalid Meta Data line. expected to find at least 8 values.');
+                    return;
+                end
+
                 this.chipID = varargin{1}(1,1);
                 this.manufacturer = varargin{1}(1,2);
                 this.pagesPerBlock = varargin{1}(1,3);
                 this.bytesPerPage = varargin{1}(1,4);
                 this.blockNumber = varargin{1}(1,5);
                 this.testID = varargin{1}(1,6);
-                
-                if size(varargin{1}, 2) >= 7
-                    this.architecture = varargin{1}(1,7);
-                else
-                    this.architecture = -1;
-                end
-                
-                if size(varargin{1}, 2) >= 8
-                    this.numOfPECycles = varargin{1}(1,8);
-                else
-                    this.numOfPECycles = -1;
-                end
-                
+                this.architecture = varargin{1}(1,7);
+                this.numOfPECycles = varargin{1}(1,8);
             else
+                if (nargin < 8)
+                    msgbox('Ivalid Meta Data line. expected to find at least 8 values.');
+                    return;
+                end
                 this.chipID = varargin{1};
                 this.manufacturer = varargin{2};
                 this.pagesPerBlock = varargin{3};
                 this.bytesPerPage = varargin{4};
                 this.blockNumber = varargin{5};
                 this.testID = varargin{6};
-                
-                if nargin >=7
-                    this.architecture = varargin{7};
-                else
-                    this.architecture = -1;
-                end
-                
-                if nargin >= 8
-                    this.numOfPECycles = varargin{8};
-                else
-                    this.numOfPECycles = -1;
-                end
+                this.architecture = varargin{7};
+                this.numOfPECycles = varargin{8};
             end 
         end
     end
